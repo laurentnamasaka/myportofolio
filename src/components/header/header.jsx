5c94821b640e913ca05d74dc50ef14a7f39544a6
laurentnamasaka <laurentnamasaka@gmail.com>
2023-03-29 10:25:58 +0300

    first commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const header = () => {
   return (
-    <div>Header</div>
+    <header>
+     <div className="container.header__container">
+          <h5>Greetings, I'm</h5>
+          <h1>Laurent Namasaka</h1>
+          <h5 className="text-light">Frontend Developer</h5>
+          <CTA />
+
+     </div>
+    </header>
   )
 }
 

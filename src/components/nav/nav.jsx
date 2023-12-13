74046a56dd918db25b71142ea00fee7b44eca214
laurentnamasaka <laurentnamasaka@gmail.com>
2023-12-13 16:56:36 +0300

    second commit

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {GiWhiteBook} from 'react-icons/gi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageRounded} from 'react-icons/bi'
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='Home'><AiOutlineHome/></a>
+      <a href='About'><AiOutlineUser/></a>
+      <a href='Experience'><GiWhiteBook/></a>
+      <a href='Services'><RiServiceLine/></a>
+      <a href='Contacts'><BiMessageRounded/></a>
+
+    </nav>
   )
 }
 
